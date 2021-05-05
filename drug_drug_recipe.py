8ea44b73900a0330fb6e5ab35b2684115847d8d3
aryehgigi <aryehgigi@gmail.com>
2021-05-05 13:28:07 +0300

    fixes

--- drug_drug_recipe.py
+++ drug_drug_recipe.py
@@ -11,6 +11,10 @@ def drug_drug_recipe(dataset, source):
         {"id": "CONTEXT", "text": "Was the context required?"},
         {"id": "TEMPO", "text": "Did the sentence contain temporal information?"}
     ]
+    
+    with open("drugs.txt") as f:
+        drugs = [l.strip().lower() for l in f.readlines()]
+    
     def get_start_offset(e, j):
         return len(" ".join(e['sentence_text'].split()[:j]))
     
@@ -28,10 +32,14 @@ def drug_drug_recipe(dataset, source):
     
     def load_my_custom_stream(s):
         examples = []
+        sents = set()
         with open(s) as f:
             reader = csv.DictReader(f)
             for line in reader:
+                if (line['sentence_text'] in sents) or (line['d1'].lower() == line['d2'].lower()):
+                    continue
                 examples.append({'sentence_text': line['sentence_text'], 'paragraph_text': line['paragraph_text'], 'd1': line['d1'], 'd1_first_index': int(line['d1_first_index']), 'd1_last_index': int(line['d1_last_index']), 'd2': line['d2'], 'd2_first_index': int(line['d2_first_index']), 'd2_last_index': int(line['d2_last_index'])})
+                sents.add(line['sentence_text'])
         return [{
                 "text": example['sentence_text'],
                 "paragraph": example['paragraph_text'][:find_sent_in_para(example['sentence_text'], example['paragraph_text'])[0]] + "<b><i>" + example['sentence_text'] + "</i></b>" + example['paragraph_text'][find_sent_in_para(example['sentence_text'], example['paragraph_text'])[1]:],
@@ -41,8 +49,7 @@ def drug_drug_recipe(dataset, source):
                     for i, tok in enumerate(example['sentence_text'].split())
                 ],
                 "spans": [
-                    {"start": get_start_offset(example, example['d1_first_index']), "end": get_start_offset(example, example['d1_first_index']) + len(example['d1']), "token_start": example['d1_first_index'], "token_end": example['d1_last_index'], "label": "DRUG"}
-                    {"start": get_start_offset(example, example['d2_first_index']), "end": get_start_offset(example, example['d2_first_index']) + len(example['d2']), "token_start": example['d2_first_index'], "token_end": example['d2_last_index'], "label": "DRUG"}
+                    {"start": get_start_offset(example, i), "end": get_start_offset(example, i) + len(tok), "token_start": i, "token_end": i, "label": "DRUG"} for i, tok in enumerate(example['sentence_text'].split()) if tok.lower() in drugs
                 ],
         } for example in examples]
     
@@ -61,7 +68,7 @@ def drug_drug_recipe(dataset, source):
         "stream": stream,
         "view_id": "blocks",
         "config": {
-            "labels": ["INTERACTION1", "INTERACTION2", "INTERACTION3", "INTERACTION4", "INTERACTION5"],
+            "labels": ["SYN1", "SYN2", "SYN3", "SYN4", "SYN5", "NON1", "NON2", "NON3", "NON4", "NON5"],
             "hide_relations_arrow": True,
             "wrap_relations": True,
             "relations_span_labels": ["DRUG"],
