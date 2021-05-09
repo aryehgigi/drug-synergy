e4c9d61cd21fa3bbb3ba9b0c4966a615419d85db
aryeht <aryeht@allenai.org>
2021-05-09 12:58:35 +0000

    started interactive multichoice

--- funcs.js
+++ funcs.js
@@ -8,19 +8,19 @@ function contextClicked() {
 }
 
 function proceed() {
-	let opts = [
-        {"id": "CONTEXT", "text": "Was the context required?"},
-        {"id": "TEMPO", "text": "Did the sentence contain temporal information?"}
-    ];
+    	let opts = []
+	window.prodigy.content["relations"].forEach((re) => {let rel = re['label']; opts.push(...[
+        	{"id": "CONTEXT" + rel, "text": "Check this if the context was required for relation " + rel + "?"},
+        	{"id": "CONTEXTNO" + rel, "text": "Otherwise, Check this if the context wasn't required for relation " + rel},
+                {"id": "TEMPO" + rel, "text": "Check this if the sentence contain temporal information relevant for " + rel},
+		{"id": "TEMPONO" + rel, "text": "Otherwise, check this if the sentence didn't contain temporal information relevant for " + rel},
+	])});
 	if ("relations" in window.prodigy.content) {
-		console.log(window.prodigy.content["relations"]);
+		console.log(window.prodigy.content["relations"][0]["label"]);
 	}
+	window.prodigy.config["blocks"].push({"view_id": "choice", "text": null, "options": opts})
+
 	window.prodigy.update({
-		"blocks": [
-			{"view_id": "relations"},
-			{"view_id": "html", "html_template": my_template(), },
-			{"view_id": "html", "html_template": my_template2(), },
-			{"view_id": "choice", "text": None, "options": opts},
-		]
+		"config": {"block": window.prodigy.config["blocks"]}
 	});
 }
