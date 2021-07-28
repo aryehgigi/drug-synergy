0e5aafca5c6bdde037cf8f1b22ed23b05d237204
aryeht <aryehgigi@gmail.com>
2021-07-28 10:48:24 +0300

    change to F score

--- prodigy2trainset.py
+++ prodigy2trainset.py
@@ -2,137 +2,9 @@ from collections import defaultdict
 import sys
 import json
 import re
-from enum import Enum
-import numpy as np
-from nltk import agreement
-# from sklearn.metrics import cohen_kappa_score
-# from statsmodels.stats.inter_rater import fleiss_kappa
+import utils
 
-
-class Label(Enum):
-    NO_COMB = 0
-    COMB_NEG = 1
-    COMB = 2
-    COMB_POS = 3
-
-
-labels = {"POS": Label.COMB_POS.value, "NEG": Label.COMB_NEG.value, "COMB": Label.COMB.value, "NO_COMB": Label.NO_COMB.value}
-labels2 = {"POS": Label.COMB_POS.value, "NEG": Label.NO_COMB.value, "COMB": Label.NO_COMB.value, "NO_COMB": Label.NO_COMB.value}
 g_anns = ['yosi', 'shaked', 'dana_a', 'dana_n', 'yuval', 'yakir', 'hagit', 'maytal']
-
-
-########################################################## Agreement ########################################################
-
-
-def get_label(rel, metric):
-    return labels[rel['class']] if "ONE_NEG" not in metric else labels2[rel['class']]
-
-
-def calc_rel_agreement(anno1, anno2, metric):
-    i = 0
-    j = 0
-    vec1 = []
-    vec2 = []
-    if "RIGOROUS" not in metric:
-        bla = set()
-        for rel1 in anno1:
-            found = False
-            for k, rel2 in enumerate(anno2):
-                if (rel1['example_hash'] != rel2['example_hash']):
-                    continue
-                rel2_spans = set(rel2['spans'])
-                spans_intersecting = 0
-                for span in rel1['spans']:
-                    for span2 in rel2_spans:
-                        if (span2[0] <= span[0] <= span2[1]) or (span2[0] <= span[1] <= span2[1]):
-                            rel2_spans.remove(span2)
-                            spans_intersecting += 1
-                            break
-                if spans_intersecting >= 2:
-                    vec1.append(get_label(rel1, metric))
-                    vec2.append(get_label(rel2, metric))
-                    if get_label(rel1, metric) != get_label(rel2, metric):
-                        print(f"same tree, diff label: {rel1} {rel2}")
-                    found = True
-                    bla.add(k)
-            if not found:
-                vec1.append(get_label(rel1, metric))
-                vec2.append(Label.NO_COMB.value)
-                if get_label(rel1, metric) != Label.NO_COMB.value:
-                    print(f"leftover rel1: {rel1}")
-        for k, rel2 in enumerate(anno2):
-            if k not in bla:
-                vec1.append(Label.NO_COMB.value)
-                vec2.append(get_label(rel2, metric))
-                if get_label(rel2, metric) != Label.NO_COMB.value:
-                    print(f"leftover rel2: {rel2}")
-        return vec1, vec2
-    while i < len(anno1) and j < len(anno2):
-        if anno1[i]['text'] == anno2[j]['text']:
-            bla = anno1[i]['text']
-        if (i == len(anno1)) or ((j < len(anno2)) and (anno1[i]['example_hash'] > anno2[j]['example_hash'])):
-            vec1.append(Label.NO_COMB.value)
-            vec2.append(get_label(anno2[j], metric))
-            j += 1
-        elif (j == len(anno2)) or ((i < len(anno1)) and (anno1[i]['example_hash'] < anno2[j]['example_hash'])):
-            vec1.append(get_label(anno1[i], metric))
-            vec2.append(Label.NO_COMB.value)
-            i += 1
-        elif anno1[i]['spans'] == anno2[j]['spans']:
-            vec1.append(get_label(anno1[i], metric))
-            vec2.append(get_label(anno2[j], metric))
-            i += 1
-            j += 1
-        # for now we treat only exact match as success,
-        #   but next i should think of how to treat the different cases (completely different/subordinate/overlap)
-        #   of the non rigorous metrics.
-        else:
-            which = 0
-            for a1, a2 in zip(list(sum(anno1[i]['spans'], ())), list(sum(anno2[j]['spans'], ()))):
-                if a1 < a2:
-                    which = 1
-                    break
-                elif a1 > a2:
-                    which = 2
-                    break
-                # here I can check if they equal but this is obviously not enough to detrmine overlap, or to know if future rel
-            if which == 0:
-                which = 1 if len(anno1[i]['spans']) < len(anno2[j]['spans']) else 2
-            if which == 2:
-                vec1.append(Label.NO_COMB.value)
-                vec2.append(get_label(anno2[j], metric))
-                j += 1
-            else:
-                vec1.append(get_label(anno1[i], metric))
-                vec2.append(Label.NO_COMB.value)
-                i += 1
-    return vec1, vec2
-
-
-def relation_agreement(rels_by_anno, anns):
-    m = [np.ones((len(anns), len(anns))), np.ones((len(anns), len(anns))), np.ones((len(anns), len(anns))), np.ones((len(anns), len(anns)))]
-    for m_i, metric in enumerate(["", "ONE_NEG"]): #, "ONE_NEG", "RIGOROUS", "RIGOROUS_ONE_NEG"]):
-        for i in range(len(anns)):
-            for j in range(len(anns)):
-                if i == j:
-                    continue
-                print([{'radio': e['radio'], 'example_hash': e['example_hash'], 'class': e['class'], 'spans': e['spans']} for e in rels_by_anno[anns[i]]])
-                print([{'example_hash': e['example_hash'], 'class': e['class'], 'spans': e['spans']} for e in rels_by_anno[anns[j]]])
-                vec1, vec2 = calc_rel_agreement(rels_by_anno[anns[i]], rels_by_anno[anns[j]], metric)
-                print(anns[i], vec1)
-                print(anns[j], vec2)
-                # score = cohen_kappa_score(vec1, vec2)
-                # use Krippendorff’s alpha
-                formatted_codes = [[1, i, vec1[i]] for i in range(len(vec1))] + [[2, i, vec2[i]] for i in range(len(vec2))]
-                ratingtask = agreement.AnnotationTask(data=formatted_codes)
-                score = ratingtask.alpha()
-                print(f"score: {score}")
-                m[m_i][i, j] = score
-    for m_i in m:
-        print(f'{"":7}', [f'{ann.split("-")[-1]:7}' for ann in anns])
-        for i, l in enumerate(m_i):
-            print(f'{anns[i].split("-")[-1]:7}', [f"{ll:7.4f}" for ll in l])
-        print()
 
 
 ########################################################## process input ########################################################
@@ -425,7 +297,7 @@ if __name__ == "__main__":
 
     # compute agreement
     if do_agreement:
-        relation_agreement(rels_by_anno, annotators)
+        utils.relation_agreement(rels_by_anno, annotators)
 
     # prepare disagreement html
     if do_agreement_html:
---
+++ utils.py
@@ -0,0 +1,105 @@
+from enum import Enum
+import numpy as np
+
+
+class Label(Enum):
+    NO_COMB = 0
+    COMB_NEG = 1
+    COMB = 2
+    COMB_POS = 3
+    NEG_AND_COMB = 4
+
+
+labels = {"POS": Label.COMB_POS.value, "NEG": Label.COMB_NEG.value, "COMB": Label.COMB.value, "NO_COMB": Label.NO_COMB.value}
+labels2 = {"POS": Label.COMB_POS.value, "NEG": Label.NEG_AND_COMB.value, "COMB": Label.NEG_AND_COMB.value, "NO_COMB": Label.NO_COMB.value}
+
+
+def get_label(rel, unify_negs):
+    return labels[rel['class']] if not unify_negs else labels2[rel['class']]
+
+
+def create_vectors(gold, test, unify_negs):
+    v_out = []
+    matched = set()
+    for rel1 in gold:
+        found = False
+        max_intersecting = 0
+        for k, rel2 in enumerate(test):
+            if rel1['example_hash'] != rel2['example_hash']:
+                continue
+            rel2_spans = set(rel2['spans'])
+            spans_intersecting = 0
+            # count matching entity-spans
+            for span in rel1['spans']:
+                for span2 in rel2_spans:
+                    # NOTE: currently we ease the case of overlapping drug names
+                    if (span2[0] <= span[0] <= span2[1]) or (span2[0] <= span[1] <= span2[1]):
+                        rel2_spans.remove(span2)
+                        spans_intersecting += 1
+                        break
+            # we have at least partial matching
+            if spans_intersecting >= 2:
+                # in case we have several partials matching the same cluster - we want to keep the maximum one
+                if spans_intersecting > max_intersecting:
+                    if max_intersecting > 0:
+                        _ = v_out.pop(-1)
+                    max_intersecting = spans_intersecting
+                v_out.append((get_label(rel1, unify_negs), get_label(rel2, unify_negs),
+                              min(spans_intersecting / len(rel1['spans']), spans_intersecting / len(rel2['spans']))))
+                found = True
+                matched.add(k)
+        # if a gold positive not found by test, add a false negative pair
+        if not found:
+            v_out.append((get_label(rel1, unify_negs), Label.NO_COMB.value, 0))
+    # no we iterate of the remaining relations in the test, and add the false positives
+    for k, rel2 in enumerate(test):
+        if k not in matched:
+            v_out.append((Label.NO_COMB.value, get_label(rel2, unify_negs), 1))
+    return v_out
+
+
+def f_from_p_r(v, labeled=False):
+    positives = len([g for (g, _, _) in v if g != Label.NO_COMB.value])
+    tp = 0
+    fp = 0
+    for g, t, s in v:
+        if ((g != 0) and (t != 0)) and ((not labeled) or (g == t)):
+            tp += s
+        elif g == 0 and t != 0:
+            fp += 1
+    p = tp / (tp + fp)
+    r = tp / positives
+    return (2 * p * r) / (p + r)
+
+
+# gold can be an annotator as reference or a data that is considered gold labeled.
+# test can be an annotator to check or a model
+def f_score(test, gold, unify_negs):
+    v = create_vectors(gold, test, unify_negs)
+    f = f_from_p_r(v)
+    f_labeled = f_from_p_r(v, labeled=True)
+    return f, f_labeled
+
+
+def relation_agreement(rels_by_anno, anns):
+    m = [np.ones((len(anns), len(anns))), np.ones((len(anns), len(anns))), np.ones((len(anns), len(anns))), np.ones((len(anns), len(anns)))]
+    for m_i, unify_negs in enumerate([False, True]):
+        sum_f = 0
+        sum_f_labeled = 0
+        for i in range(len(anns)):
+            for j in range(len(anns)):
+                if i == j:
+                    continue
+                f, f_labeled = f_score(rels_by_anno[anns[i]], rels_by_anno[anns[j]], unify_negs)
+                sum_f += f
+                sum_f_labeled += f_labeled
+                m[m_i][i, j] = f
+                m[m_i + 2][i, j] = f_labeled
+        print(f"averaged unlabeled F1 score {'where POS vs NEG+COMB ' if unify_negs else ''}= {sum_f / (len(anns) * (len(anns) - 1))}")
+        print(f"averaged labeled F1 score {'where POS vs NEG+COMB ' if unify_negs else ''}= {sum_f_labeled / (len(anns) * (len(anns) - 1))}")
+    for i, m_i in enumerate([m[0], m[2], m[1], m[3]]):
+        print(f"This is {'POS vs NEG+COMB ' if i > 1 else ''}{'unlabeled' if i % 2 != 0 else 'labeled'} pairwise F1 score table:")
+        print(f'{"":7}', [f'{ann.split("-")[-1]:7}' for ann in anns])
+        for i, l in enumerate(m_i):
+            print(f'{anns[i].split("-")[-1]:7}', [f"{ll:7.4f}" for ll in l])
+        print()
